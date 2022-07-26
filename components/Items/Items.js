f7d98a45af181c1badc331578d17bb0c88a776dd
Virginia Dana Rostagno <95722444+VdRostagno@users.noreply.github.com>
2022-07-26 10:52:43 -0300

    Add files via upload

---
+++ components/Items/Items.js
@@ -0,0 +1,28 @@
+import React from "react"
+import {Link} from "react-router-dom";
+
+import Col from "react-bootstrap/Col";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+
+
+function Items({id,nombre,imagen}){
+    return ( 
+       
+                    <Col >
+                        <Card >
+                            <Card.Img className="imagen-moto" variant="top" src={imagen} />
+                            <Card.Body className="cuerpo-carta">
+                            <Card.Title className="nombre-moto">{nombre}</Card.Title>
+
+                            <Card.Text>
+                               
+                             </Card.Text>
+                             <Link to={`/detalles/${id}`} className="boton-detalle"><Button variant="outline-dark" className="boton-detalle" > Ver Detalles </Button></Link>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                
+    )
+}
+export default Items
---
+++ firebase/config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDVu_YNOnH0FvxvQjzKOoJkzBRwhACrYok",
+  authDomain: "rostagnoreact-54e20.firebaseapp.com",
+  projectId: "rostagnoreact-54e20",
+  storageBucket: "rostagnoreact-54e20.appspot.com",
+  messagingSenderId: "939213339466",
+  appId: "1:939213339466:web:c792e158ba402896ccdfb3"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
